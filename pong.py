121cd0fff2e937068d1b445d15c824a8f178b099
ryan-charette <ryanacharette@gmail.com>
2022-05-15 16:31:27 -0500

    Update pong.py
    - Added score and scoreboard
    - Game ends when either play reaches 11 points
    - Ball speeds up when returned
    - Divider drawn in center of screen

--- pong.py
+++ pong.py
@@ -1,11 +1,36 @@
-from random import randint
 import turtle
+from random import randint
+import time
 
 play_window = turtle.Screen()
 play_window.title("Pong")
 play_window.bgcolor("black")
 play_window.setup(width = 800, height = 600)
 play_window.tracer(0)
+
+left_score = 0
+right_score = 0
+
+score = turtle.Turtle()
+score.speed(0)
+score.color("white")
+score.penup()
+score.hideturtle()
+score.goto(0, 210)
+score.write("0       0", align = "center", font = ("Courier", 50, "normal"))
+
+divider = turtle.Turtle()
+divider.speed(0)
+divider.color("white")
+divider.right(90)
+divider.penup()
+divider.goto(0, 300)
+divider.pendown()
+for dash in range(31):
+    divider.forward(15)
+    divider.penup()
+    divider.forward(5)
+    divider.pendown()
 
 left_paddle = turtle.Turtle()
 left_paddle.speed(0)
@@ -35,26 +60,39 @@ if ball.x_velocity < 0.02 and ball.x_velocity >= 0:
 elif ball.x_velocity < 0 and ball.x_velocity > -0.02:
     ball.x_velocity = -0.02
 ball.y_velocity = randint(-5, 5) / 100
+if ball.y_velocity == 0:
+    ball.y_velocity = 0.01
 
 def left_paddle_up():
     y = left_paddle.ycor()
-    y += 20
-    left_paddle.sety(y)
+    if y < 250:
+        y += 20
+        left_paddle.sety(y)
+    else:
+        left_paddle.sety(250)
 
 def left_paddle_down():
     y = left_paddle.ycor()
-    y -= 20
-    left_paddle.sety(y)
+    if y > -250:
+        y -= 20
+        left_paddle.sety(y)
+    else:
+        left_paddle.sety(-250)
 
 def right_paddle_up():
     y = right_paddle.ycor()
-    y += 20
-    right_paddle.sety(y)
-
+    if y < 250:
+        y += 20
+        right_paddle.sety(y)
+    else:
+        right_paddle.sety(250)
 def right_paddle_down():
     y = right_paddle.ycor()
-    y -= 20
-    right_paddle.sety(y)
+    if y > -250:
+        y -= 20
+        right_paddle.sety(y)
+    else:
+        right_paddle.sety(-250)
 
 play_window.listen()
 play_window.onkeypress(left_paddle_up, "w")
@@ -62,7 +100,7 @@ play_window.onkeypress(left_paddle_down, "s")
 play_window.onkeypress(right_paddle_up, "Up")
 play_window.onkeypress(right_paddle_down, "Down")
 
-while True:
+while left_score < 11 and right_score < 11:
     play_window.update()
 
     ball.setx(ball.xcor() + ball.x_velocity)
@@ -77,14 +115,25 @@ while True:
     
     if ball.xcor() > 390:
         ball.goto(0, 0)
-        ball.x_velocity *= -1
+        ball.x_velocity = -1 * randint(2, 5) / 100
+        left_score += 1
+        score.clear()
+        score.write("{}      {}".format(left_score, right_score), align = "center", font = ("Courier", 50, "normal"))
+
     elif ball.xcor() < -390:
         ball.goto(0, 0)
-        ball.x_velocity *= -1
+        ball.x_velocity = randint(2, 5) / 100
+        right_score += 1
+        score.clear()
+        score.write("{}      {}".format(left_score, right_score), align = "center", font = ("Courier", 50, "normal"))
         
     if ball.xcor() > 360 and ball.xcor() < 370 and ball.ycor() < right_paddle.ycor() + 50 and ball.ycor() > right_paddle.ycor() - 50:
         ball.setx(360)
-        ball.x_velocity *= -1
+        ball.x_velocity *= -1.25
     elif ball.xcor() < -360 and ball.xcor() > -370 and ball.ycor() < left_paddle.ycor() + 50 and ball.ycor() > left_paddle.ycor() - 50:
         ball.setx(-360)
-        ball.x_velocity *= -1
+        ball.x_velocity *= -1.25
+    if left_score == 11 or right_score == 11:
+        score.clear()
+        score.write("GAME OVER", align = "center", font = ("Courier", 50, "normal"))
+        time.sleep(2)
